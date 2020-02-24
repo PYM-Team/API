1d97c6425735290c2d0c8ea97fe0b7ca6ea98b3e
gagadu71 <gaetan@falcand.nom.fr>
2020-02-24 15:45:41 +0100

    #3 add route to get Players linked to a specified game

--- src/routes/games.route.js
+++ src/routes/games.route.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router';
-import { helloWorld, createGame, getGame, createPlayer } from '../controllers/games.controller';
+import { helloWorld, createGame, getGame, createPlayer, getPlayers } from '../controllers/games.controller';
 
 const router = new Router();
 
@@ -11,10 +11,13 @@ router.get('/', helloWorld);
 // GET
 router.get('/games/:id', getGame);
 
+// GET
+router.get('/players/gameid/:gameId', getPlayers);
+
 // POST
 router.post('/games', createGame);
 
 // POST
-router.post('/players/:gameId', createPlayer);
+router.post('/players/gameid/:gameId', createPlayer);
 
 export default router;
