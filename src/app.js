3eb9c3ae96f7e398ef7dac262009d7389fba89b0
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-03-23 19:54:47 +0100

    #9 Check if the player or game exist before adding it in DB

--- src/app.js
+++ src/app.js
@@ -28,38 +28,76 @@ app.context.io = io;
 
 io.on('connection', (socket) => {
   let gameId = 0;
+  let playerUsername;
 
   console.log('Connection established with client : ', socket.id);
 
   socket.on('createGame', (roomId) => {
     gameId = roomId;
-    Game({ id: roomId, usernames: [] }).save();
-    socket.join(gameId);
-    console.log(`Create real time for the room ${roomId}`);
+    Game.find({ id: roomId })
+      .then((games) => {
+        if (games.length == 0) {
+          console.log("OK on peut créer une game");
+          Game({ id: roomId, usernames: [] }).save();
+          socket.join(gameId);
+          console.log(`Create real time for the room ${roomId}`);
+        } else {
+          socket.join(gameId);
+          console.log(`Create real time for the room ${roomId}`);
+        }
+      })
+      .catch((err) => { console.log(err) });
+
   });
 
-  socket.on('connectGame', (roomId) => {
+  socket.on('connectGame', (roomId, username) => {
+
+    // dynamically store connection information in the server
     gameId = roomId;
+    playerUsername = username;
+
+    // Add the player to the right socket.io group
     socket.join(`${gameId}player`);
     console.log('player connected to roomId');
-    Player({ gameId: roomId, name: 'toto' }).save()
-      .then(() => {
-        Player.find({ gameId: roomId })
-          .then((players, err) => {
-            console.log(players);
-            io.to(gameId).emit('playerConnected', players);
-            Game.findOneAndUpdate({ id: roomId }, { $push: { usernames: players.name } });
-          });
+
+    // TODO: Check if the gameid exist !
+    // Check if the player does not already exist in the database
+    Player.find({ gameId: roomId, name: username })
+      .then((player) => {
+        // If not
+        console.log('inside the if');
+        if (player.length == 0) {
+          console.log(`add a new player to the database with game id ${roomId}`);
+          // Add it
+          Player({ gameId: roomId, name: playerUsername }).save()
+            .then(() => {
+              //Then we get every players from the game and push the new player inside
+              Player.find({ gameId: roomId })
+                .then((players) => {
+                  console.log(players);
+                  io.to(gameId).emit('playerConnected', players);
+                  Game.findOneAndUpdate({ id: roomId }, { $push: { usernames: username } }); // !not working as expected
+                })
+                .catch((err) => { console.log(err) });
+            });
+        } else {
+          console.log(`The player ${username} just reconnected, he already is in database`);
+          console.log(player);
+          Player.find({ gameId: roomId })
+            .then((players) => {
+              io.to(gameId).emit('playerConnected', players);
+              Game.findOneAndUpdate({ id: roomId }, { $push: { usernames: username } }); // !not working as expected
+            })
+            .catch((err) => { console.log(err) });
+        }
       })
-      .catch((err) => {
-      });
-  });
+      .catch((err) => { console.log(err) });
+  })
 
   socket.on('disconnect', (reason) => {
     io.to(gameId).emit('playerDisconnected');
   });
 });
-
 
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
