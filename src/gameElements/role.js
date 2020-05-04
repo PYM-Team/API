c05896313fd841b4dc44873d0dba5cd314536b72
gagadu71 <gaetan@falcand.nom.fr>
2020-05-04 16:06:20 +0200

    #19 add list of basic objects to a role

--- src/gameElements/role.js
+++ src/gameElements/role.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 /**
- * Definition d'une structure de base d'une role du jeu
+ * Definition d'une structure de base d'un role du jeu
  * @param
  */
 export class Role {
-  constructor(name, description, mission) {
+  constructor(name, description, mission, basicObjets) {
     this.name = name || null;
     this.description = description || null;
     this.mission = mission || null;
+    this.basicObjets = basicObjets || null;
   }
 }
