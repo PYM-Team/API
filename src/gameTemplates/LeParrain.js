82177f540621b7f3a8fb59d1b76d029c8546bbb0
gagadu71 <gaetan@falcand.nom.fr>
2020-05-04 17:01:20 +0200

    #19 add objects of the scenario

--- src/gameTemplates/LeParrain.js
+++ src/gameTemplates/LeParrain.js
@@ -6,6 +6,99 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+// Objects
+  // El Sampico
+const PoemeAmour = new Object(
+  'Poème d\'amour', 
+  'Un long poème dans lequel El Sampico a mis son don pour l’écriture au service de sa plume afin d’y déverser toute l’intensité de son amour pour Carla, la femme du boss.', 
+  true
+);
+const NoteHotel = new Object(
+  'Note de l\'hôtel',
+  'Une note de l’hôtel où il a passé la nuit avec Carla le soir du meurtre. Elle aurait été l’alibi parfait si elle ne révélait pas en même temps sa relation secrète et incriminante avec sa dulcinée.',
+  false
+);
+const PaquetCigarettes1 = new Object(
+  'Paquet de cigarettes', 
+  'Un paquet presque vide de cigarettes italiennes sans filtre au goût amer. El Sampico ne sait pas quoi faire de ses mains tant qu’elles ne sont pas occupées à tenir une clope. ', 
+  true
+);
+const FlasqueTequila = new Object(
+  'Flasque de Tequila', 
+  'Un petit remontant pour se donner du courage.', 
+  false
+);
+const Flingue1 = new Object(
+  'Flingue',
+  'Pour El Sampico, cette arme est une extension de sa volonté.', 
+  false
+);
+const SouFetiche = new Object(
+  'Sou fétiche', 
+  'Un peso brillant lui rappelant sa terre natale, le Mexique.',
+  false
+);
+const basicObjectsSampico = [PoemeAmour, NoteHotel, PaquetCigarettes1, FlasqueTequila, Flingue1, SouFetiche];
+  // Petro Francesco
+const PaquetCigarettes2 = new Object(
+  'Paquet de cigarettes', 
+  'Déjà à moitié vide en début de soirée, de la marque italienne Fortuna.',
+  false
+);
+const Briquet = new Object(
+  'Briquet', 
+  'Un vieux briquet en acier noir, lourdement usé par l’utilisation quotidienne de son propriétaire.', 
+  false
+);
+const LunettesSoleil = new Object(
+  'Paire de lunettes de soleil', 
+  'Lunettes rondes et sombre qui cache à la perfection le regard mystérieux et ténébreux de l’officier de police. Il les porte même en intérieur, ce qui lui permet de regarder là ou il veut sans se faire facilement repérer.', 
+  false
+);
+const BlocNote = new Object(
+  'Bloc-note', 
+  'Un bon enquêteur à toujours de quoi noter les information les plus intéressantes qui lui sont proposée. Mais ce carnet n’est pas celui où Petro garde toutes ses notes concernant son enquête… Il serait bien trop risqué de l\'amener à ce genre de soirée.', 
+  false
+);
+const Menottes = new Object(
+  'Paire de menottes', 
+  'Peuvent être utiliser au cour de la soirée, Petro les cache à l’intérieur de sa veste', 
+  false
+);
+const basicObjectsFrancesco = [PaquetCigarettes2, Briquet, LunettesSoleil, BlocNote, Menottes];
+  // Vito Falcaninio
+const LettreParrain = new Object(
+  'La lettre du parrain', 
+  'Cette lettre signée du parrain exprime ses doutes sur son entourage direct. C’est elle qui pousse Vito a réunir tout ce beau monde ce soir', 
+  true
+);
+const Cigare = new Object(
+  'Un cigare allumé', 
+  '“Jamais sans mon cubain. J’en ai besoin pour réfléchir, et réfléchir je fais que ça.”', 
+  false
+);
+const Flingue2 = new Object(
+  'Flingue', 
+  'Comme le couteau du boucher ou la plume de l’écrivain : l’outil de travail.', 
+  false
+);
+const ChiffonEnsenglante = new Object(
+  'Un chiffon ensanglanté', 
+  'Vito a saigné du nez récemment, rien de grave', 
+  false
+);
+const basicObjectsFalcaninio = [LettreParrain, Cigare, Flingue2, ChiffonEnsenglante];
+  // Tommaso-Giorgio
+const Truc = new Object(
+  '', 
+  '', 
+  false
+);
+
+
+
+// Places
+
 // Actions
 const fouillerPiece = (_, place) => {
   switch (place) {
@@ -112,11 +205,11 @@ const missionSampico = new Mission(
 );
 
 // Create roles in the game
-currentGame.addRole('Vito Falcaninio', 'le bras droit', missionVito);
-currentGame.addRole('Carla Gurzio', 'la femme du parrain');
-currentGame.addRole('Petro Francesco', 'l\'infiltré de la police locale');
-currentGame.addRole('Sebastiano Pechetto', 'dit le “Chimiste”, responsable des labos de synthèse de drogue');
-currentGame.addRole('Tommaso-Giorgio', 'fils du parrain');
-currentGame.addRole('"El Sampico"', 'le meilleur tueur à gage');
+currentGame.addRole('Vito Falcaninio', 'le bras droit', missionVito, basicObjectsFalcaninio);
+currentGame.addRole('Carla Gurzio', 'la femme du parrain', missionCarla);
+currentGame.addRole('Petro Francesco', 'l\'infiltré de la police locale', missionPetro, basicObjectsFrancesco);
+currentGame.addRole('Sebastiano Pechetto', 'dit le “Chimiste”, responsable des labos de synthèse de drogue', missionSebastiano);
+currentGame.addRole('Tommaso-Giorgio', 'fils du parrain', missionTommaso);
+currentGame.addRole('"El Sampico"', 'le meilleur tueur à gage', missionSampico, basicObjectsSampico);
 
 export default currentGame;
