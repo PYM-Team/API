87d7a740cd696090d97c6308ea19d763e9411206
gagadu71 <gaetan@falcand.nom.fr>
2020-05-04 16:05:45 +0200

    #19 create object class

---
+++ src/gameElements/object.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+/**
+ * Definition d'une structure de base d'un objet du jeu
+ * @param
+ */
+export class Object {
+    constructor(name, description, clue) {
+        this.name = name;
+        this.desciption = description || null;
+        this.clue = clue || null;
+    }
+}
