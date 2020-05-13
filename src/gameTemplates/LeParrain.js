43ac73d50aa3d3a8d9e58691767d7094df15f6eb
gagadu71 <gaetan@falcand.nom.fr>
2020-05-13 15:34:49 +0200

    #19 creation of propper objects GamaObject and add the places to the current game

--- src/gameTemplates/LeParrain.js
+++ src/gameTemplates/LeParrain.js
@@ -14,155 +14,48 @@ function getRandomInt(max) {
 
 // Objects
 // El Sampico
-const PoemeAmour = {
-  name: 'Poème d\'amour',
-  description: 'Un long poème dans lequel El Sampico a mis son don pour l’écriture au service de sa plume afin d’y déverser toute l’intensité de son amour pour Carla, la femme du boss.',
-  clue: true,
-};
-const NoteHotel = {
-  name: 'Note de l\'hôtel',
-  description: 'Une note de l’hôtel où il a passé la nuit avec Carla le soir du meurtre. Elle aurait été l’alibi parfait si elle ne révélait pas en même temps sa relation secrète et incriminante avec sa dulcinée.',
-  clue: true,
-};
-const PaquetCigarettes1 = {
-  name: 'Paquet de cigarettes',
-  description: 'Un paquet presque vide de cigarettes italiennes sans filtre au goût amer. El Sampico ne sait pas quoi faire de ses mains tant qu’elles ne sont pas occupées à tenir une clope. ',
-  clue: false,
-};
-const FlasqueTequila = {
-  name: 'Flasque de Tequila',
-  description: 'Un petit remontant pour se donner du courage.',
-  clue: false,
-};
-const Flingue1 = {
-  name: 'Flingue',
-  description: 'Pour El Sampico, cette arme est une extension de sa volonté.',
-  clue: false,
-};
-const SouFetiche = {
-  name: 'Sou fétiche',
-  description: 'Un peso brillant lui rappelant sa terre natale, le Mexique.',
-  clue: false,
-};
+const PoemeAmour = new GameObject('Poème d\'amour', 'Un long poème dans lequel El Sampico a mis son don pour l’écriture au service de sa plume afin d’y déverser toute l’intensité de son amour pour Carla, la femme du boss.', true);
+const NoteHotel = new GameObject('Note de l\'hôtel', 'Une note de l’hôtel où il a passé la nuit avec Carla le soir du meurtre. Elle aurait été l’alibi parfait si elle ne révélait pas en même temps sa relation secrète et incriminante avec sa dulcinée.', true);
+const PaquetCigarettes1 = new GameObject('Paquet de cigarettes', 'Un paquet presque vide de cigarettes italiennes sans filtre au goût amer. El Sampico ne sait pas quoi faire de ses mains tant qu’elles ne sont pas occupées à tenir une clope. ', false);
+const FlasqueTequila = new GameObject('Flasque de Tequila', 'Un petit remontant pour se donner du courage.', false);
+const Flingue1 = new GameObject('Flingue', 'Pour El Sampico, cette arme est une extension de sa volonté.', false);
+const SouFetiche = new GameObject('Sou fétiche', 'Un peso brillant lui rappelant sa terre natale, le Mexique.', false);
 const basicObjectsSampico = [PoemeAmour, NoteHotel, PaquetCigarettes1, FlasqueTequila, Flingue1,
   SouFetiche];
 // Petro Francesco
-const PaquetCigarettes2 = {
-  name: 'Paquet de cigarettes',
-  description: 'Déjà à moitié vide en début de soirée, de la marque italienne Fortuna.',
-  clue: false,
-};
-const Briquet = {
-  name: 'Briquet',
-  descritpion: 'Un vieux briquet en acier noir, lourdement usé par l’utilisation quotidienne de son propriétaire.',
-  clue: false,
-};
-const LunettesSoleil = {
-  name: 'Paire de lunettes de soleil de rechange',
-  description: 'Une paire de lunettes rondes et sombres',
-  clue: false,
-};
-const BlocNote = {
-  name: 'Bloc-note',
-  description: 'Un bloc notes avec plein de notes griffonnées incompréhensibles, à part pour les noms de toutes les personnes de la soirée !',
-  clue: false,
-};
-const Menottes = {
-  name: 'Paire de menottes',
-  description: 'Une simple paire de menottes',
-  clue: true,
-};
+const PaquetCigarettes2 = new GameObject('Paquet de cigarettes', 'Déjà à moitié vide en début de soirée, de la marque italienne Fortuna.', false);
+const Briquet = new GameObject('Briquet', 'Un vieux briquet en acier noir, lourdement usé par l’utilisation quotidienne de son propriétaire.', false);
+const LunettesSoleil = new GameObject('Paire de lunettes de soleil de rechange', 'Une paire de lunettes rondes et sombres', false);
+const BlocNote = new GameObject('Bloc-note', 'Un bloc notes avec plein de notes griffonnées incompréhensibles, à part pour les noms de toutes les personnes de la soirée !', false);
+const Menottes = new GameObject('Paire de menottes', 'Une simple paire de menottes', true);
 const basicObjectsFrancesco = [PaquetCigarettes2, Briquet, LunettesSoleil, BlocNote, Menottes];
 // Vito Falcaninio
-const LettreParrain = {
-  name: 'La lettre du parrain',
-  description: 'Cette lettre signée du parrain exprime ses doutes sur son entourage direct. C’est elle qui pousse Vito a réunir tout ce beau monde ce soir',
-  clue: true,
-};
-const Cigare = {
-  name: 'Un cigare allumé',
-  description: '“Jamais sans mon cubain. J’en ai besoin pour réfléchir, et réfléchir je fais que ça.”',
-  clue: false,
-};
-const Flingue2 = {
-  name: 'Flingue',
-  description: 'Comme le couteau du boucher ou la plume de l’écrivain : l’outil de travail.',
-  clue: false,
-};
+const LettreParrain = new GameObject('La lettre du parrain', 'Cette lettre signée du parrain exprime ses doutes sur son entourage direct. C’est elle qui pousse Vito a réunir tout ce beau monde ce soir', true);
+const Cigare = new GameObject('Un cigare allumé', '“Jamais sans mon cubain. J’en ai besoin pour réfléchir, et réfléchir je fais que ça.”', false);
+const Flingue2 = new GameObject('Flingue', 'Comme le couteau du boucher ou la plume de l’écrivain : l’outil de travail.', false);
 const basicObjectsFalcaninio = [LettreParrain, Cigare, Flingue2];
 // Tommaso-Giorgio
-const Addition = {
-  name: 'Addition de restaurant',
-  description: 'Ce petit bout de papier indique que Tomasso était au restaurant au moment où le parrain a été assassiné. D’après le prix et le menu, il semble qu’il y soit allé tout seul.',
-  clue: true,
-};
-const MontreGousset = {
-  name: 'Montre à gousset',
-  description: 'Une jolie montre gravée qui vaut sûrement une fortune.',
-  clue: false,
-};
-const Chevaliere = {
-  name: 'Une chevalière en argent',
-  description: 'Une imposante bague en argent sous la forme d’un serpent qui se mord la queue.',
-  clue: false,
-};
+const Addition = new GameObject('Addition de restaurant', 'Ce petit bout de papier indique que Tomasso était au restaurant au moment où le parrain a été assassiné. D’après le prix et le menu, il semble qu’il y soit allé tout seul.', true);
+const MontreGousset = new GameObject('Montre à gousset', 'Une jolie montre gravée qui vaut sûrement une fortune.', false);
+const Chevaliere = new GameObject('Une chevalière en argent', 'Une imposante bague en argent sous la forme d’un serpent qui se mord la queue.', false);
 const basicObjectsTommaso = [Addition, MontreGousset, Chevaliere];
 // Carla Gurzio
-const Photographie = {
-  name: 'Photographie de El Sampico',
-  description: 'Dans le portefeuille de Carla se trouve une magnifique photographie noir et blanc du tueur à gages Mexicain. Sa moustache foisonnante rayonne de par son sourire chaleureux. La photographie semble usée par le temps mais reste malgré tout en bon état.',
-  clue: true,
-};
-const FlasqueParfum = {
-  name: 'Flasque de parfum',
-  description: 'Une petite fiole de parfum à la rose de couleur vive.',
-  clue: 'false',
-};
-const Peigne = {
-  name: 'Peigne en or',
-  description: 'Magnifique pièce en or gravé en forme de petit peigne. Semble avoir été utilisé à de nombreuse reprise.',
-  clue: 'false',
-};
-const Carnet = {
-  name: 'Carnet',
-  description: 'Un carnet de note à la couverture en cuir de crocodile et coloré rouge vif. Il est presque vide et on peut y lire quelques date importante et rendez-vous sans grand intérêt avec des personnalités qui ne vous disent rien… On y trouve aussi quelques numéros de téléphone.',
-  clue: false,
-};
+const Photographie = new GameObject('Photographie de El Sampico', 'Dans le portefeuille de Carla se trouve une magnifique photographie noir et blanc du tueur à gages Mexicain. Sa moustache foisonnante rayonne de par son sourire chaleureux. La photographie semble usée par le temps mais reste malgré tout en bon état.', true);
+const FlasqueParfum = new GameObject('Flasque de parfum', 'Une petite fiole de parfum à la rose de couleur vive.', false);
+const Peigne = new GameObject('Peigne en or', 'Magnifique pièce en or gravé en forme de petit peigne. Semble avoir été utilisé à de nombreuse reprise.', false);
+const Carnet = new GameObject('Carnet', 'Un carnet de note à la couverture en cuir de crocodile et coloré rouge vif. Il est presque vide et on peut y lire quelques date importante et rendez-vous sans grand intérêt avec des personnalités qui ne vous disent rien… On y trouve aussi quelques numéros de téléphone.', false);
 const basicObjectsCarla = [Photographie, FlasqueParfum, Peigne, Carnet];
 // Sebastiano Pechetto
-const RapportMorgue = {
-  name: 'Rapport de la morgue',
-  description: 'Ce rapport indique que la parrain a été empoisonné.',
-  clue: true,
-};
-const GantsLatex = {
-  name: 'Une paire de gants en latex',
-  description: 'Une vieille paire de gants tachés.',
-  clue: false,
-};
-const Cocaine = {
-  name: 'Cocaïne',
-  description: 'Une petit sachet transparent de 2 grammes de cocaïne. De quoi se donner un bon  coup de fouet.',
-  clue: false,
-};
+const RapportMorgue = new GameObject('Rapport de la morgue', 'Ce rapport indique que la parrain a été empoisonné.', true);
+const GantsLatex = new GameObject('Une paire de gants en latex', 'Une vieille paire de gants tachés.', false);
+const Cocaine = new GameObject('Cocaïne', 'Une petit sachet transparent de 2 grammes de cocaïne. De quoi se donner un bon  coup de fouet.', false);
 const basicObjectsSebastiano = [RapportMorgue, GantsLatex, Cocaine];
 
 // Places
-const ChambreParrain = {
-  name: 'La chambre du parrain',
-  description: '',
-  objects: [],
-};
-const Labo = {
-  name: 'Le laboratoire du chimiste',
-  description: '',
-  objects: [],
-};
-const Vestibule = {
-  name: 'Le vestibule',
-  description: '',
-  objects: [],
-};
+
+currentGame.addPlace('La chambre du parrain', '', []);
+currentGame.addPlace('Le laboratoire du chimiste', '', []);
+currentGame.addPlace('Le vestibule', '', []);
 
 // Relations
 // El Sampico
@@ -217,8 +110,8 @@ const relationsSebastiano = (
 
 // Actions
 /**
- * 
- * @param {*} _ 
+ *
+ * @param {*} _
  * @param {*} place prend en argument l'objet Place qui correspond à la pièce voulant être fouillée
  */
 const fouillerPiece = (_, place) => {
