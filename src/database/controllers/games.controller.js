0a74091abad6ac66ace8c5de05f940ccb878b535
gagadu71 <gaetan@falcand.nom.fr>
2020-05-28 20:43:00 +0200

    #28 add the games in the database to the wobsocket

--- src/database/controllers/games.controller.js
+++ src/database/controllers/games.controller.js
@@ -18,39 +18,54 @@ export function getGame(gameToGet) {
   });
 }
 
+export function getAllGames() {
+  return new Promise((resolve, reject) => {
+    const games = [];
+    Game.find({}, (err, game) => {
+      games[game.gameId] = game;
+    })
+      .then(() => {
+        resolve(games);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
 export function gameIdExist(aGameId) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     Game.exists({ gameId: aGameId })
       .then((result) => {
         resolve(result);
       })
       .catch((err) => {
-        resolve(err);
+        reject(err);
       });
   });
 }
 
 export function createGame(gameToCreate, aGameId) {
   // generating the random id;
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     Game({ game: gameToCreate, gameId: aGameId }).save()
       .then(() => {
         resolve();
       })
       .catch((err) => {
-        resolve(err);
+        reject(err);
       });
   });
 }
 
 export function deleteGame(aGameId) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     Game.deleteOne({ gameId: aGameId })
       .then(() => {
         resolve();
       })
       .catch((err) => {
-        resolve(err);
+        reject(err);
       });
   });
 }
